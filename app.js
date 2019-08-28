f0d7a4b4c765a12b14579301b3324065dbfbe338
sandyyy007 <sandy335582@gmail.com>
2019-08-28 12:29:02 +0530

    added index.php

--- app.js
+++ app.js
@@ -1,28 +1,61 @@
-getWeather();
-document.getElementById('city').addEventListener('change' , getWeather);
 
-function getWeather(){
-var city =document.getElementById('city').value;
-var tempDOM=document.getElementById('temp');
-var humidityDOM=document.getElementById('humidity');
-var descDOM=document.getElementById('description');
-var videoDiv=document.getElementById('videoDiv');
-var request=`https://api.apixu.com/v1/current.json?key=28cfb531d99948bc8e6172719191607&q=${city}`;
-var main;
-fetch(request)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(myJson){
-      console.log(myJson);
-        current=myJson.current;
-        tempDOM.textContent=current.temp_c;
-        humidityDOM.textContent=current.humidity;
-        descDOM.textContent=current.condition.text;
-        document.getElementById('imgg').src=current.condition.icon;
-        videoDiv.innerHTML=`<video id="bgVideo" autoplay="true" loop="true" src="${descDOM.textContent}.mov"></video>`
-        document.getElementById('bgVideo').play();
+document.getElementById('city').addEventListener('change' , getAsyncWeather)
 
-        })
+//using async await
+async function getAsyncWeather(){
+
+  var city =document.getElementById('city').value;
+  var tempDOM=document.getElementById('temp');
+  var humidityDOM=document.getElementById('humidity');
+  var descDOM=document.getElementById('description');
+  var videoDiv=document.getElementById('videoDiv');
+  var request=`https://api.apixu.com/v1/current.json?key=28cfb531d99948bc8e6172719191607&q=${city}`;
+  const response= await fetch(request);
+  const myJson= await response.json();
+
+  current=myJson.current;
+  tempDOM.textContent=current.temp_c;
+  humidityDOM.textContent=current.humidity;
+  descDOM.textContent=current.condition.text;
+  document.getElementById('imgg').src=current.condition.icon;
+  console.log(current.condition.icon);
+  videoDiv.innerHTML=`<video id="bgVideo" autoplay="true" loop="true" src="${descDOM.textContent}.mov"></video>`
+  document.getElementById('bgVideo').play();
+  
+
+
+
 
 }
+
+
+
+//with promises only 
+
+// function getWeather(){
+
+// var city =document.getElementById('city').value;
+// var tempDOM=document.getElementById('temp');
+// var humidityDOM=document.getElementById('humidity');
+// var descDOM=document.getElementById('description');
+// var videoDiv=document.getElementById('videoDiv');
+// var request=`https://api.apixu.com/v1/current.json?key=28cfb531d99948bc8e6172719191607&q=${city}`;
+
+// fetch(request)
+//     .then(function(response){
+//       //console.log(response.json());
+//       return response.json();
+//     })
+//     .then(function(myJson){
+//       console.log(myJson);
+//         current=myJson.current;
+//         tempDOM.textContent=current.temp_c;
+//         humidityDOM.textContent=current.humidity;
+//         descDOM.textContent=current.condition.text;
+//         document.getElementById('imgg').src=current.condition.icon;
+//         videoDiv.innerHTML=`<video id="bgVideo" autoplay="true" loop="true" src="${descDOM.textContent}.mov"></video>`
+//         document.getElementById('bgVideo').play();
+
+//         })
+
+// }
